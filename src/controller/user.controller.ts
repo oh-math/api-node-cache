bc74d36c566744dba7e12c8a4b803133ec04ef61
Matheus <math.av7@outlook.com>
2023-02-05 23:17:07 -0300

    refactor: using res.send instead res.json

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response, Router } from "express";
-import { createUser, deleteUser, getUser } from "../service/user.service";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser
+} from "../service";
 
 const router = Router();
 
 router.post("/users", async (req: Request, res: Response) => {
   try {
     const result = await createUser(req.body);
-    res.json({ result });
+    res.send(result);
   } catch (error) {
     res.status(404);
     console.error(error);
@@ -16,7 +22,28 @@ router.post("/users", async (req: Request, res: Response) => {
 router.get("/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await getUser(req.params.id);
-    res.json({ result });
+    res.send(result);
+  } catch (error) {
+    res.status(404);
+    console.error(error);
+  }
+});
+
+router.get("/users/", async (req: Request, res: Response) => {
+  try {
+    const result = await getAllUsers();
+    res.send(result);
+    res.status(200);
+  } catch (error) {
+    res.status(404);
+    console.error(error);
+  }
+});
+
+router.put("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await updateUser(req.params.id, req.body);
+    res.send(result);
   } catch (error) {
     res.status(404);
     console.error(error);
