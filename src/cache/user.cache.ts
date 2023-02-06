7c8c28d8eff6017687284f9673c77287a01da902
Matheus <math.av7@outlook.com>
2023-02-05 22:59:33 -0300

    refactor: creating better get cache function

--- src/cache/user.cache.ts
+++ src/cache/user.cache.ts
@@ -1,15 +1,22 @@
 import Cache from "node-cache";
 
-const userCache = new Cache();
+export const userCache = new Cache();
 
-export const set = (key: string, value, ttl = 3000) => {
+export const set = (key: string, value: any, ttl = 3000) => {
   return userCache.set(key, value, ttl);
-};
-
-export const get = (key: string) => {
-  return userCache.get(key);
 };
 
 export const clearAllCache = () => {
   return userCache.flushAll();
+};
+
+export const getCached = (key: string): [] | null => {
+  const result = userCache.get(key);
+
+  if (!result) {
+    return null;
+  }
+
+  console.log("Using cache to return");
+  return result as [];
 };
