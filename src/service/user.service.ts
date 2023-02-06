a35a1b80957ace28827bfa254c309174a2919db2
Matheus <math.av7@outlook.com>
2023-02-05 23:18:23 -0300

    feat<cache>: implementing cache in `user.service`

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,12 @@
 import { User } from "@prisma/client";
+import { getCached, set } from "../cache/user.cache";
 import prisma from "../prisma/prisma.service";
+
+const KEY = "user-list";
+
+const count = async (): Promise<number> => {
+  return await prisma.user.count();
+};
 
 export const createUser = async (user: any) => {
   const { email, name, posts } = user || {};
@@ -17,6 +24,49 @@ export const createUser = async (user: any) => {
 
 export const getUser = async (id: string): Promise<User | null> => {
   const result = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  return result;
+};
+
+export const getAllUsers = async (): Promise<User[] | [] | null> => {
+  let userCache: [] | null = getCached(KEY);
+
+  const countUser = await count();
+  const userCacheLength = userCache?.length || 0;
+
+  if (countUser > userCacheLength || countUser < userCacheLength) {
+    const result = await prisma.user.findMany({
+      include: {
+        posts: {},
+      },
+    });
+
+    set(KEY, result);
+    userCache = getCached(KEY);
+
+    console.log('QUERY FEITA');
+    
+    return userCache;
+  }
+
+  return userCache;
+};
+
+export const updateUser = async (
+  id: string,
+  input: any
+): Promise<User | null> => {
+  const { email, name } = input || {};
+
+  const result = await prisma.user.update({
+    data: {
+      email,
+      name,
+    },
     where: {
       id: parseInt(id),
     },
